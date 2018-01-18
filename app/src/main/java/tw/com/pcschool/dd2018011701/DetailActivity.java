e7f6b772ff7b8cf55568d9b3ae9acf6adf10cd52
Teacher <shaojung@gmail.com>
2018-01-18 09:29:23 +0800

    Back and Delete

--- app/src/main/java/tw/com/pcschool/dd2018011701/DetailActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018011701/DetailActivity.java
@@ -1,7 +1,10 @@
 package tw.com.pcschool.dd2018011701;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import tw.com.pcschool.dd2018011701.data.Student;
@@ -9,11 +12,12 @@ import tw.com.pcschool.dd2018011701.data.Student;
 public class DetailActivity extends AppCompatActivity {
     Student s;
     TextView tv1, tv2, tv3;
+    int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        int id = getIntent().getIntExtra("id", 0);
+        id = getIntent().getIntExtra("id", 0);
         s = MainActivity.dao.getStudent(id);
         tv1 = (TextView) findViewById(R.id.textView);
         tv2 = (TextView) findViewById(R.id.textView2);
@@ -21,5 +25,31 @@ public class DetailActivity extends AppCompatActivity {
         tv1.setText(String.valueOf(s.id));
         tv2.setText(s.name);
         tv3.setText(String.valueOf(s.score));
+    }
+    public void clickBack(View v)
+    {
+        finish();
+    }
+    public void clickDelete(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(DetailActivity.this);
+        builder.setTitle("刪除確認");
+        builder.setMessage("確認要刪除本筆資料嗎?");
+        builder.setPositiveButton("確認", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                MainActivity.dao.delete(id);
+                finish();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.show();
+
+
     }
 }
